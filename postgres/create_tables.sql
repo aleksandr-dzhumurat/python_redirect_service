2f3adb7c558e5079248a3c33ecdd3f322cb51d73
adzhumurat <dju0204@gmail.com>
2023-11-27 09:27:34 +0300

    Add postgres backend

---
+++ postgres/create_tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT exists short_links (
+    origin_url varchar(400),
+    short_url varchar(50)
+);
--- src/utils.py
+++ src/utils.py
@@ -2,43 +2,56 @@ import hashlib
 import json
 import os
 import logging
+import time
 
 
 logger = logging.getLogger('my_logger')
 logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO)
 
 
-# # Dictionary to store your redirection links
-# redirection_links = {
-#     "example": "https://www.example.com",
-#     "google": "https://www.google.com",
-#     # Add more redirection links as needed
-# }
-
-
 class LinkShortener:
     def __init__(self):
+        # TODO implement STORAGE interface
         self.db_file = '/srv/data/links_db.json'
         self.cache = {}
 
     def init_db(self):
-        if os.path.exists(self.db_file):
-            with open(self.db_file, 'r') as f:
-                self.cache = json.load(f)
+        # TODO implement STORAGE interface
+        if os.environ['STORAGE_BACKEND'] == 'json':
+            if os.path.exists(self.db_file):
+                with open(self.db_file, 'r') as f:
+                    self.cache = json.load(f)
+        elif os.environ['STORAGE_BACKEND'] == 'postgres':
+            from src.postgres_backend import read_links
+
+            self.cache = read_links()
+        else:
+            raise RuntimeError("Specify runtime backend")
 
     def persist(self):
-        if len(self.cache) > 0:
-            with open(self.db_file, 'w') as f:
-                json.dump(self.cache, f)
+        # TODO: add FastAPI backkground task
+        # PERSIST_PERIOD_SEC = 30
+        # current_epoch_time = int(time.time())
+        # TODO implement STORAGE interface
+        if os.environ['STORAGE_BACKEND'] == 'json':
+            if len(self.cache) > 0:
+                with open(self.db_file, 'w') as f:
+                    json.dump(self.cache, f)
+        elif os.environ['STORAGE_BACKEND'] == 'postgres':
+            from src.postgres_backend import insert_links
+
+            insert_links(self.cache)
+        else:
+            raise RuntimeError("Specify runtime backend")
 
     def generate_link(self, url: str):
         if url in self.cache:
-            page_link = self.cache[url]
+            short_url = self.cache[url]
         else:
-            page_link = str(hashlib.md5(url.encode('utf-8')).hexdigest())[:6]
-            self.cache[page_link] = url
+            short_url = str(hashlib.md5(url.encode('utf-8')).hexdigest())[:10]
+            self.cache[short_url] = url
             self.persist()
-        return page_link
+        return short_url
 
     def get_origin_link(self, short_url: str):
         if short_url in self.cache:
@@ -51,8 +64,12 @@ class LinkShortener:
         num_links = 10
         links = list(self.cache.keys())[:10]
         res = {}
+        cnt = 0
         for k in links:
+            if cnt > 10:
+                break
             res[k] = self.cache[k]
+            cnt += 1
         return res
 
 
